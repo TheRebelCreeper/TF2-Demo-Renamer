66c92d6816c27b71f17af33980398de76182cc69
Aaron Lampert <therebelcreeper@gmail.com>
2024-09-21 22:34:34 -0500

    New logic for reading contents of demo file to check for EOF

--- Rename_Demos/src/main/Demo.java
+++ Rename_Demos/src/main/Demo.java
@@ -22,19 +22,19 @@ public class Demo
 	{
 		path = Demo.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		path = URLDecoder.decode(path, "UTF-8");
-	    path = path.substring(0, path.lastIndexOf("/")) + "/";
+		path = path.substring(0, path.lastIndexOf("/")) + "/";
 	    
 		File rootDir = new File(path);
 		
 		for (File demo: rootDir.listFiles())
 		{
+			System.out.println(demo.getName());
 			// If the file name has no letters except for the extension
 			if (demo.getName().contains(".dem")&& Pattern.matches("([a-zA-Z]*)(_?)([^a-zA-Z]+)", demo.getName().replace(".dem", "")))
 			{
 				mapName = readDemo(demo);
 				name = demo.getName().replace(".dem", "");
 				File newDemo = new File(path + name + "_" + mapName + ".dem");
-				//File newDemo = new File(path + TimeConvert(name) + "_" + mapName + ".dem");
 				checkJson(demo, path);
 				demo.renameTo(newDemo);
 			}
@@ -48,32 +48,8 @@ public class Demo
 		{
 			name = json.getName().replace(".json", "");
 			File newJson = new File(path + name + "_" + mapName + ".json");
-			//File newJson = new File(path + TimeConvert(name) + "_" + mapName + ".json");
 			return json.renameTo(newJson);
 		}
-	}
-	
-	private String TimeConvert(String tmp)
-	{
-		String date = tmp.substring(0, tmp.indexOf("_") + 1);	// YYYY-MM-DD_hh-mm-ss
-		String hour = tmp.substring(tmp.indexOf("_") + 1, tmp.indexOf("_") + 3);
-		String end = tmp.substring(tmp.indexOf("_") + 3);
-		
-		int intHour = new Integer(hour);
-		
-		String pm = (intHour < 12) ? "_am" : "_pm";
-		
-		if (intHour % 12 == 0)
-			intHour = 12;
-		else
-			intHour %= 12;
-		
-		hour = "" + intHour;
-		if (intHour < 10)
-			hour = "0" + hour;
-		
-		return date + hour + end;
-		//return date + hour + end + pm;
 	}
 	
 	private String readDemo(File demo) throws IOException
@@ -81,12 +57,17 @@ public class Demo
 		String tmp = "";
 		FileReader file = new FileReader(demo);
 		char[] cbuf = new char[65536];
-		while(!tmp.contains(".bsp"))
+		System.out.println("START");
+		while (file.read(cbuf, 0, 65536) != -1)
 		{
-			file.read(cbuf, 0, 65536);
 			tmp = new String(cbuf);
+			if (tmp.contains(".bsp"))
+			{
+				file.close();
+				return tmp.substring(tmp.indexOf("maps\\") + 5, tmp.indexOf(".bsp"));
+			}
 		}
 		file.close();
-		return tmp.substring(tmp.indexOf("maps\\") + 5, tmp.indexOf(".bsp"));
+		return "error";
 	}
 }
